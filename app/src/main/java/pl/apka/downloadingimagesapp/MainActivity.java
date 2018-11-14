6a52f3e387e6789a718c1fb84751aadbcfed014e
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-11-14 17:56:24 +0100

    manifest added

--- app/src/main/java/pl/apka/downloadingimagesapp/MainActivity.java
+++ app/src/main/java/pl/apka/downloadingimagesapp/MainActivity.java
@@ -1,19 +1,73 @@
 package pl.apka.downloadingimagesapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+
     public void downloadImage (View view) {
-        Log.i("info", "Button tapped");
+        ImageDownloader task = new ImageDownloader();
+
+        Bitmap myImage;
+
+        try {
+            myImage = task.execute("").get();
+
+            imageView.setImageBitmap(myImage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+    }
+
+    public  class  ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.connect();
+
+                InputStream in = connection.getInputStream();
+
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+
+                return bitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return  null;
+
+            }
+
+        }
     }
 }
