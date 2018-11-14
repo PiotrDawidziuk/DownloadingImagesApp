44288599c168e089c0d784f98b71c18a99d44109
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-11-14 17:15:16 +0100

    downloadImage function added

--- app/src/main/java/pl/apka/downloadingimagesapp/MainActivity.java
+++ app/src/main/java/pl/apka/downloadingimagesapp/MainActivity.java
@@ -2,8 +2,14 @@ package pl.apka.downloadingimagesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void downloadImage (View view) {
+        Log.i("info", "Button tapped");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
